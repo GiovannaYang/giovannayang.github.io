d9e14739d16eb1acff824e79f353e9154796b64d
yqr533 <yqr533@gmail.com>
2023-08-23 15:00:07 +0800

    Commit

--- astro.config.ts
+++ astro.config.ts
@@ -6,7 +6,7 @@ import { remarkPlugins, rehypePlugins } from "./plugins";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://zxh.io/",
+  site: "https://qirongy.github.io",
   integrations: [solid(), unocss({ injectReset: true }), sitemap()],
   markdown: {
     remarkPlugins,
